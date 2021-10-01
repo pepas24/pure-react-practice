15d4f7f407f46116b40314929740191a5ef57ad2
Sebastian Gutierrez <sebastian@rocketfy.co>
2021-10-01 18:02:53 -0500

    Hello World exercise, part 2

--- exercises/hello-world/src/index.js
+++ exercises/hello-world/src/index.js
@@ -2,12 +2,19 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import { Hello } from "./Hello";
+import { World } from "./World";
+
 // A React component it's just a function that returns JSX, also can be writen
 // as a Class using ES6 sintax, but Function Components are more used nowdays
+// The name of component must begin with an uppercase letter, due to an lowercase
+// name will be treated as a built-in HTML tag.
 function HelloWorld() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <h1>
+        <Hello/>{' '}<World/>
+      </h1>
     </div>
     // The above is JSX, it's a sintax created by React to write HTML in a more
     // conveniet way this will be converted by Babel into
@@ -15,9 +22,18 @@ function HelloWorld() {
     //      'div',
     //      {},
     //      React.createElement(
-    //          'h1',
-    //          {},
-    //          'Hello world'
+    //        'h1',
+    //        {},
+    //        React.createElement(
+    //            Hello,
+    //            {},
+    //            null
+    //        ),
+    //        React.createElement(
+    //            World,
+    //            {},
+    //            null
+    //        ),
     //      )
     //  )
   );
